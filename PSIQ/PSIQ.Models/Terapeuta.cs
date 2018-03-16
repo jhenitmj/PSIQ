c1b3326b1c07cbef18fb57a1849953a3baa18b2a
jhenitmj <jheni.tmj@outlook.com>
2018-03-15 22:06:12 -0300

    Finalizando tela login terapeuta

---
+++ PSIQ/PSIQ.Models/Terapeuta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSIQ.Models
+{
+    public class Terapeuta
+    {
+        public string   Nome        { get; set; }
+        public int      CRP         { get; set; }
+        public string   Senha       { get; set; }
+        public DateTime DtNasciento { get; set; }
+        public int      Idade       { get; set; }
+    }
+}
