69473616278aaddbebf7111dfd83c181e3626904
Tiago Andrade <tgnandrade@gmail.com>
2018-04-12 18:01:41 -0300

    upload de fotos
    Identification: 22
    Change-Id: I0d4a834118e6384d1c33429689d264b07365fcee

--- PSIQ/PSIQ.WebUI/Controllers/PreCadastroPacienteController.cs
+++ PSIQ/PSIQ.WebUI/Controllers/PreCadastroPacienteController.cs
@@ -1,5 +1,8 @@
 ﻿using PSIQ.DataAccess;
 using PSIQ.Models;
+using System;
+using System.IO;
+using System.Web;
 using System.Web.Mvc;
 
 namespace PSIQ.WebUI.Controllers
@@ -9,7 +12,6 @@ namespace PSIQ.WebUI.Controllers
         public ActionResult Index()
         {
             ViewBag.Estados = new EstadoDAO().BuscarTodos();
-            ViewBag.Diagnostico = new DiagnosticoDAO().BuscarTodos();
             return View();
         }
 
@@ -17,8 +19,32 @@ namespace PSIQ.WebUI.Controllers
         {
             new PacienteDAO().Inserir(obj);
 
-            //Redirecionando para a tela de listagem de Cidades
             return RedirectToAction("Index", "PerfilTera");
+        }
+
+        [HttpPost]
+        public JsonResult Upload()
+        {
+            try
+            {
+                if (!Directory.Exists(Server.MapPath("~/Images")))
+                    Directory.CreateDirectory(Server.MapPath("~/Images"));
+
+                foreach (string fileName in Request.Files)
+                {
+                    HttpPostedFileBase f = Request.Files[fileName];
+                    string savedFileName = Path.Combine(Server.MapPath("~/Images"), Path.GetFileName(f.FileName));
+                    FileInfo fi = new FileInfo(savedFileName);
+                    f.SaveAs(savedFileName);
+                    return Json(fi.Name);
+                }
+
+                return Json(null);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
