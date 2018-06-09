264bc17dabfce257e0039d241b2e5afb2b4f507b
jhenitmj <jheni.tmj@outlook.com>
2018-06-08 22:28:05 -0300

    comi

--- PSIQ/DiagXPaciTest/Test1.cs
+++ PSIQ/DiagXPaciTest/Test1.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using PSIQ.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DiagXPaciTest
-{
-    class Test1
-    {
-    }
-}
+//namespace DiagXPaciTest
+//{
+////    class Test1
+////    {
+////        Diagnostico obj = new Diagnostico();
+
+////        public string SalvarDiag;
+
+
+
+////    }
+////}
