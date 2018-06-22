efb028cb2672f913ab3af362c533e2c9ce968ecc
IsabelaSucharski <jheni.tmj@outlook.com>
2018-06-21 22:21:13 -0300

    Ultimas correções

--- PSIQ/PSIQ.WebUI/Controllers/CadTeraController.cs
+++ PSIQ/PSIQ.WebUI/Controllers/CadTeraController.cs
@@ -21,6 +21,13 @@ namespace PSIQ.WebUI.Controllers
         {
             if (ModelState.IsValid)
             {
+                
+
+                if (!ValidarEmail(obj.Email))
+                {
+                    ViewBag.MsgErro = "E-mail inválido!";
+                    return View("Index");
+                }
                 var u = Mapper.Map<TeraViewModel, Usuario>(obj);
                 u.Tipo = TIPO_USUARIO.TERAPEUTA;
               
@@ -53,6 +60,29 @@ namespace PSIQ.WebUI.Controllers
             {
                 throw ex;
             }
+
+        }
+     
+
+        public bool ValidarEmail(string email)
+        {
+            if (String.IsNullOrEmpty(email))
+                return false;
+            if (!email.Contains("@") || !email.Contains("."))
+                return false;
+            string[] strCamposEmail = email.Split(new String[] { "@" }, StringSplitOptions.RemoveEmptyEntries);
+            if (strCamposEmail.Length != 2)
+                return false;
+            if (strCamposEmail[0].Length < 3)
+                return false;
+            if (!strCamposEmail[1].Contains("."))
+                return false;
+            strCamposEmail = strCamposEmail[1].Split(new String[] { "." }, StringSplitOptions.RemoveEmptyEntries);
+            if (strCamposEmail.Length < 2)
+                return false;
+            if (strCamposEmail[0].Length < 1)
+                return false;
+            return true;
         }
     }
 }
--- PSIQ/PSIQ.WebUI/Controllers/CadastroPController.cs
+++ PSIQ/PSIQ.WebUI/Controllers/CadastroPController.cs
@@ -30,6 +30,17 @@ namespace PSIQ.WebUI.Controllers
         {
             if (ModelState.IsValid)
             {
+                if (!IsValidCPF(obj.CPF))
+                {
+                    ViewBag.MsgErro = "CPF inválido!";
+                    return View("Index");
+                }
+
+                if (!ValidarEmail(obj.Email))
+                {
+                    ViewBag.MsgErro = "E-mail inválido!";
+                    return View("Index");
+                }
                 var u = Mapper.Map<PaciViewModel, Usuario>(obj);
                 u.Tipo = TIPO_USUARIO.PACIENTE;
 
@@ -67,6 +78,77 @@ namespace PSIQ.WebUI.Controllers
             {
                 throw ex;
             }
+        }
+
+        private bool IsValidCPF(string cpf)
+        {
+            string valor = cpf.Replace(".", "");
+            valor = valor.Replace("-", "");
+
+            if (valor.Length != 11)
+                return false;
+
+            bool igual = true;
+            for (int i = 1; i < 11 && igual; i++)
+                if (valor[i] != valor[0])
+                    igual = false;
+
+            if (igual || valor == "12345678909")
+                return false;
+
+            int[] numeros = new int[11];
+            for (int i = 0; i < 11; i++)
+                numeros[i] = int.Parse(valor[i].ToString());
+
+            int soma = 0;
+            for (int i = 0; i < 9; i++)
+                soma += (10 - i) * numeros[i];
+
+            int resultado = soma % 11;
+            if (resultado == 1 || resultado == 0)
+            {
+                if (numeros[9] != 0)
+                    return false;
+            }
+            else if (numeros[9] != 11 - resultado)
+                return false;
+
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += (11 - i) * numeros[i];
+            resultado = soma % 11;
+
+            if (resultado == 1 || resultado == 0)
+            {
+                if (numeros[10] != 0)
+                    return false;
+            }
+            else
+                if (numeros[10] != 11 - resultado)
+                return false;
+
+            return true;
+        }
+
+        public bool ValidarEmail(string email)
+        {
+            if (String.IsNullOrEmpty(email))
+                return false;
+            if (!email.Contains("@") || !email.Contains("."))
+                return false;
+            string[] strCamposEmail = email.Split(new String[] { "@" }, StringSplitOptions.RemoveEmptyEntries);
+            if (strCamposEmail.Length != 2)
+                return false;
+            if (strCamposEmail[0].Length < 3)
+                return false;
+            if (!strCamposEmail[1].Contains("."))
+                return false;
+            strCamposEmail = strCamposEmail[1].Split(new String[] { "." }, StringSplitOptions.RemoveEmptyEntries);
+            if (strCamposEmail.Length < 2)
+                return false;
+            if (strCamposEmail[0].Length < 1)
+                return false;
+            return true;
         }
     }
 }
